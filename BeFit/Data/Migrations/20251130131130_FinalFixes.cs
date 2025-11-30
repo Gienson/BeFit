b5fd7b5355f09bf52fbc9cabaf864c193c27eba4
Gienson <jakub.gierusz@wp.pl>
2025-11-30 14:15:42 +0100

    Małe poprawki.

---
+++ BeFit/Data/Migrations/20251130131130_FinalFixes.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BeFit.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FinalFixes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "BestResult",
+                table: "Stat",
+                newName: "TotalReps");
+
+            migrationBuilder.AddColumn<double>(
+                name: "AvgWeight",
+                table: "Stat",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MaxWeight",
+                table: "Stat",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AvgWeight",
+                table: "Stat");
+
+            migrationBuilder.DropColumn(
+                name: "MaxWeight",
+                table: "Stat");
+
+            migrationBuilder.RenameColumn(
+                name: "TotalReps",
+                table: "Stat",
+                newName: "BestResult");
+        }
+    }
+}
